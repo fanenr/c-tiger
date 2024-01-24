7cdad51cca599b8161ad3dae4db3f8db77758faf
Arthur63 <Arthur63@163.com>
2024-01-24 10:47:48 +0800

    add util

---
+++ util.c
@@ -0,0 +1,44 @@
+#include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *
+checked_strdup (const char *src)
+{
+  size_t cap = strlen (src) + 1;
+  char *ret = malloc (cap);
+  if (ret == NULL)
+    goto err;
+  if (memcpy (ret, src, cap) != ret)
+    goto err;
+  return ret;
+err:
+  fprintf (stderr, "string failed\n");
+  exit (1);
+}
+
+void *
+checked_alloc (size_t size)
+{
+  void *ret = malloc (size);
+  if (ret == NULL)
+    {
+      fprintf (stderr, "malloc failed\n");
+      exit (1);
+    }
+  memset (ret, 0, size);
+  return ret;
+}
+
+void *
+checked_realloc (void *ptr, size_t size)
+{
+  void *ret = realloc (ptr, size);
+  if (ret == NULL)
+    {
+      fprintf (stderr, "realloc failed\n");
+      exit (1);
+    }
+  return ret;
+}
